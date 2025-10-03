a08794f44b976fb69bf3adf14c3db88a9c38f888
tomchccom <dreamkms2014@gmail.com>
2025-10-03 23:50:29 +0900

    chore : Post 응답 DTO명 변경

---
+++ backend/src/main/java/com/example/tomo/Users/dtos/ResponsePostUniformDto.java
@@ -0,0 +1,18 @@
+<<<<<<<< HEAD:backend/src/main/java/com/example/tomo/Users/dtos/ResponseUniformDto.java
+========
+package com.example.tomo.Users.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponsePostUniformDto {
+    private Boolean success;
+    private String message;
+}
+>>>>>>>> c9abc16 (chore : Post 응답 DTO명 변경):backend/src/main/java/com/example/tomo/Users/dtos/ResponsePostUniformDto.java
