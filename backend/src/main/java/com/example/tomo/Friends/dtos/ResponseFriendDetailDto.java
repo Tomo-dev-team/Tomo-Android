c666ed60ff3df0e9841a7659c1660d5367cac836
tomchccom <dreamkms2014@gmail.com>
2025-10-03 23:14:06 +0900

    chore : 디렉토리 구조 변경으로 삭제된 DTO 복구

---
+++ backend/src/main/java/com/example/tomo/Friends/dtos/ResponseFriendDetailDto.java
@@ -0,0 +1,24 @@
+package com.example.tomo.Friends.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+// 이 DTO 는 그냥 응답용으로 사용하고
+@NoArgsConstructor
+public class ResponseFriendDetailDto {
+
+    private String username;
+    private Double friendship;
+    private String createdAt;
+
+    public ResponseFriendDetailDto(String username, Double friendship, String createdAt) {
+        this.username = username;
+        this.friendship = friendship;
+        this.createdAt = createdAt;
+    }
+
+}
---
+++ backend/src/main/java/com/example/tomo/Friends/dtos/ResponseGetFriendsDto.java
@@ -0,0 +1,13 @@
+package com.example.tomo.Friends.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseGetFriendsDto {
+    private String name;
+
+}
