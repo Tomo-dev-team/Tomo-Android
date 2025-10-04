1008deb8765c5ea62f75b3a664e2de492c34e58e
tomchccom <dreamkms2014@gmail.com>
2025-10-04 17:31:21 +0900

    fix(controller) : jwt 검증이 필요한 API 요청에 /public 계층 추가 - userController 를 제외한 모든 컨트롤러에 @RequestMapping("/public") 추가

--- backend/src/main/java/com/example/tomo/Friends/FriendController.java
+++ backend/src/main/java/com/example/tomo/Friends/FriendController.java
@@ -5,6 +5,7 @@ import com.example.tomo.global.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.tomo.Friends.dtos.ResponseFriendDetailDto;
 
@@ -12,6 +13,7 @@ import com.example.tomo.Friends.dtos.ResponseFriendDetailDto;
 import java.util.List;
 
 @RestController
+@RequestMapping("/public")
 public class FriendController {
 
     private final FriendService friendService;
