fde20bb2fc929e5d8eb602f8fd778000640c8e8a
mark <mark77234@naver.com>
2025-10-19 16:12:13 +0900

    feat: step1 색션 부가설명추가

--- frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/home/meeting/steps/Step1Section.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/home/meeting/steps/Step1Section.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.markoala.tomoandroid.ui.components.CustomText
 import com.markoala.tomoandroid.ui.components.CustomTextField
+import com.markoala.tomoandroid.ui.components.CustomTextType
+import com.markoala.tomoandroid.ui.theme.CustomColor
 
 @Composable
 fun StepOneSection(
@@ -17,11 +21,23 @@ fun StepOneSection(
 ) {
     Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
 
+        CustomText(
+            text = "모임 제목",
+            type = CustomTextType.title,
+            color = CustomColor.gray300,
+            fontSize = 16.sp
+        )
         CustomTextField(
             value = moimName,
             onValueChange = onNameChange,
             modifier = Modifier.fillMaxWidth(),
             placeholder = "모임명을 입력해주세요"
+        )
+        CustomText(
+            text = "모임 설명",
+            type = CustomTextType.title,
+            color = CustomColor.gray300,
+            fontSize = 16.sp
         )
         CustomTextField(
             value = description,
