2f22ecff71c66dd90d6af14caee9a2c5d772340e
mark <mark77234@naver.com>
2025-10-19 16:52:31 +0900

    feat: step1 디자인 수정

--- frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/home/meeting/steps/Step1Section.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/home/meeting/steps/Step1Section.kt
@@ -3,6 +3,7 @@ package com.markoala.tomoandroid.ui.main.home.meeting.steps
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,13 +20,16 @@ fun StepOneSection(
     onNameChange: (String) -> Unit,
     onDescriptionChange: (String) -> Unit
 ) {
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+    Column(
+        modifier = Modifier.padding(vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
 
         CustomText(
             text = "모임 제목",
-            type = CustomTextType.title,
-            color = CustomColor.gray300,
-            fontSize = 16.sp
+            type = CustomTextType.body,
+            color = CustomColor.black,
+            fontSize = 14.sp
         )
         CustomTextField(
             value = moimName,
@@ -34,10 +38,10 @@ fun StepOneSection(
             placeholder = "모임명을 입력해주세요"
         )
         CustomText(
-            text = "모임 설명",
-            type = CustomTextType.title,
-            color = CustomColor.gray300,
-            fontSize = 16.sp
+            text = "모임 제목",
+            type = CustomTextType.body,
+            color = CustomColor.black,
+            fontSize = 14.sp
         )
         CustomTextField(
             value = description,
