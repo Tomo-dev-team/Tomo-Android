c6562cdb34545786e6a2a8ea5ca699e41bc223f8
mark <mark77234@naver.com>
2025-11-18 14:29:18 +0900

    feat: 버전 업데이트

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.markoala.tomoandroid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
