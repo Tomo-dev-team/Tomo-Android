18fdd2f3feb9e15023e412bdd92b0cba7caad7c5
tomchccom <dreamkms2014@gmail.com>
2025-12-20 16:30:00 +0900

    Refactor : create dto validation 적용

--- backend/src/main/java/com/example/tomo/Moim/dtos/addMoimRequestDto.java
+++ backend/src/main/java/com/example/tomo/Moim/dtos/addMoimRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.tomo.Moim.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,12 @@ import java.util.List;
 @Setter
 public class addMoimRequestDto {
 
+    @NotBlank(message = "모임명이 누락되었습니다.")
     private String title;
-    private String description; //
+
+    @NotBlank(message = "모임 설명이 누락되었습니다.")
+    private String description;
+
+    @NotBlank(message = "모임 참가자 이메일이 누락되었습니다.")
     private List<String> emails; // 사용자 이름 , 이메일로
 }
--- backend/src/main/java/com/example/tomo/Users/dtos/RequestUserSignDto.java
+++ backend/src/main/java/com/example/tomo/Users/dtos/RequestUserSignDto.java
@@ -1,5 +1,7 @@
 package com.example.tomo.Users.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RequestUserSignDto {
 
+    @NotBlank(message = "uid가 누락되었습니다.")
     private String uuid;
+
+    @Email (message = "올바른 이메일 형식이 아닙니다.")
+    @NotBlank(message = "email이 누락되었습니다")
     private String email;
+
+    @NotBlank(message = "사용자가 누락되었습니다.")
     private String username;
 
 }
