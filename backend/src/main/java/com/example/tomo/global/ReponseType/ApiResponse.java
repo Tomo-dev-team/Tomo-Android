0d996e2433aeb10682e8c889aab84725246cc4a1
myeongsung kim <134512691+tomchaccom@users.noreply.github.com>
2025-12-21 22:17:03 +0900

    refactor : 회의 후 수정사항 반영 (#120)
    * fix : 포인트컷 범위 수정
    
    * fix : 응답 수정
    
    * fix : gitignore 수정
    
    ---------
    
    Co-authored-by: tomchccom <dreamkms2014@gmail.com>

--- backend/src/main/java/com/example/tomo/global/ReponseType/ApiResponse.java
+++ backend/src/main/java/com/example/tomo/global/ReponseType/ApiResponse.java
@@ -14,7 +14,6 @@ import lombok.Setter;
 public class ApiResponse<T> {
 
     private boolean success;
-    private String code;
     private String message;
     private T data;
 
@@ -22,11 +21,11 @@ public class ApiResponse<T> {
     // 성공 응답 생성용
     public static <T> ApiResponse<T> success(T data, String message) {
 
-        return new ApiResponse<>(true,"SUCCESS", message, data);
+        return new ApiResponse<>(true, message, data);
     }
 
-    public static <T> ApiResponse<T> failure(String code, String message) {
-        return new ApiResponse<>(false, code, message, null);
+    public static <T> ApiResponse<T> failure( String message) {
+        return new ApiResponse<>(false, message, null);
     }
 
 }
