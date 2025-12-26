7b9677c7e15dec941fb37ff0da1719bc0a3db614
tomchccom <dreamkms2014@gmail.com>
2025-12-26 18:44:42 +0900

    feat: getter 추가

--- backend/src/main/java/com/example/tomo/global/Embedded/Location.java
+++ backend/src/main/java/com/example/tomo/global/Embedded/Location.java
@@ -2,9 +2,12 @@ package com.example.tomo.global.Embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 
 @Embeddable
+@Getter
 public class Location {
 
     @Column(precision = 9, scale = 6)
