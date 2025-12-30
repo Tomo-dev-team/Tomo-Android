065e571766eaf6fb14920f40017320bbe0ccc851
mark <mark77234@naver.com>
2025-12-30 20:36:12 +0900

    refactor: 백엔드 폴더 제거

--- backend/src/main/java/com/example/tomo/global/SelfFriendRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.tomo.global;
-
-public class SelfFriendRequestException extends RuntimeException {
-    public SelfFriendRequestException(String message) {
-        super(message);
-    }
-}
